43343aaadf1c3627071554078d94d9c9b6091514
wtokarz80 <w.tokarz80gmail.com>
2020-08-19 14:39:34 +0200

    add task 3 from sql queries

--- SQL_queries.sql
+++ SQL_queries.sql
@@ -8,3 +8,11 @@ SELECT "role", count(*), CAST((ROUND(count(role_id) * 100/ (SELECT COUNT("role")
 FROM users INNER JOIN roles on users.role_id = roles.id
 GROUP BY "role"
 ORDER BY "count" ASC;
+
+SELECT roles.role,
+    sum(case when users.isactive = TRUE then 1 else 0 end) AS active,
+    sum(case when users.isactive = FALSE then 1 else 0 end) AS not_active,
+	count(*) AS total
+FROM users INNER JOIN roles on users.role_id = roles.id
+GROUP BY role
+ORDER BY role ASC;
