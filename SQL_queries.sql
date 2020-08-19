2d84787d7a4837edd383304e46856f58ecc6f6a1
Aldona <aldonapawlak2@gmail.com>
2020-08-19 14:21:55 +0200

    remove invalid file

--- SQL_queries.sql
+++
@@ -1,9 +0,0 @@
-SELECT "role", count (*) AS total
-FROM users INNER JOIN "role" on users.role_id = "role".id
-GROUP BY "role"
-ORDER BY "total" ASC;
-
-SELECT "role", count(*), CAST((ROUND(count(role_id) * 100/ (SELECT COUNT("role") FROM users INNER JOIN "role" on users.role_id = "role".id))) AS varchar(100)) || '%' AS percentages
-FROM users INNER JOIN "role" on users.role_id = "role".id
-GROUP BY "role"
-ORDER BY "count" ASC;
