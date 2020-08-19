7bd67e87a2c3d2bb030d70484bc2a356be33cda6
Aldona <aldonapawlak2@gmail.com>
2020-08-19 15:30:48 +0200

    add sql query for task number 7

--- SQL_queries.sql
+++ SQL_queries.sql
@@ -23,3 +23,14 @@ SELECT modules.name, count (*) AS total
 FROM students INNER JOIN modules on students.module_id = modules.id
 GROUP BY modules.name
 ORDER BY total ASC;
+
+
+
+
+
+
+
+// task number 7
+SELECT "status", COUNT(*) AS total, CAST((ROUND(count(quest_statuses.id) * 100/ (SELECT COUNT("status") FROM student_quests INNER JOIN quest_statuses on student_quests.quest_status_id = quest_statuses.id))) AS varchar(100)) || '%' AS percentages
+FROM student_quests INNER JOIN quest_statuses on student_quests.quest_status_id = quest_statuses.id
+GROUP BY "status";
