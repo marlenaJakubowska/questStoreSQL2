44431eb66dd52af85cd8987fb8174d2104020e83
Aldona <aldonapawlak2@gmail.com>
2020-08-19 14:32:31 +0200

    add sql query for second task

--- SQL_queries.sql
+++ SQL_queries.sql
@@ -3,3 +3,8 @@ FROM users INNER JOIN roles on users.role_id = roles.id
 GROUP BY "role"
 ORDER BY "total";
 
+
+SELECT "role", count(*), CAST((ROUND(count(role_id) * 100/ (SELECT COUNT("role") FROM users INNER JOIN roles on users.role_id = roles.id))) AS varchar(100)) || '%' AS percentages
+FROM users INNER JOIN roles on users.role_id = roles.id
+GROUP BY "role"
+ORDER BY "count" ASC;
