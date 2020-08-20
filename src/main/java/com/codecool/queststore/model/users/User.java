022f5cb40d4ad7000c41d3625a89686639a72002
wtokarz80 <w.tokarz80gmail.com>
2020-08-20 17:20:03 +0200

    refactor model Student

--- src/main/java/com/codecool/queststore/model/users/User.java
+++ src/main/java/com/codecool/queststore/model/users/User.java
@@ -6,13 +6,13 @@ import com.codecool.queststore.model.Reward;
 import java.util.List;
 
 public abstract class User {
-    private final int id;
-    private final String firstName;
-    private final String lastName;
-    private final Role role;
+    private int id;
+    private String firstName;
+    private String lastName;
+    private Role role;
     private boolean isActive;
-    private final String login;
-    private final String password;
+    private String login;
+    private String password;
 
 
 
@@ -26,5 +26,42 @@ public abstract class User {
         this.isActive = isActive;
         this.login = login;
         this.password = password;
+    }
+
+    public User() {
+
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
     }
 }
