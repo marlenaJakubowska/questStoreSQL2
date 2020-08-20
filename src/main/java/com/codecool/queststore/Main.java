8f50c99cc24af602221842ca3ba2b26a3cb78ca2
wtokarz80 <w.tokarz80gmail.com>
2020-08-20 15:16:46 +0200

    main function refactored

--- src/main/java/com/codecool/queststore/Main.java
+++ src/main/java/com/codecool/queststore/Main.java
@@ -9,13 +9,6 @@ import com.codecool.queststore.model.users.Role;
 public class Main {
     public static void main(String[] args) {
 
-//        PostgresDao test = new PostgresDao();
-//        test.setConnection();
-//        test.closeConnection();
 
-
-    int name = 1;
-    Quest quest = new Quest(1, "quest 1", 500, ModuleName.valueOf(name), "dupa");
-        System.out.println(quest);
     }
 }
